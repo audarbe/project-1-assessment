6aed61cce3e092f54c5b774542ecb0e1b405f240
Alexei Udarbe <alexeiudarbe@AU-MacBook-Bro.local>
2020-05-15 14:05:54 -0700

    complete

--- js/main.js
+++ js/main.js
@@ -1,5 +1,40 @@
 /*----- constants -----*/
+
 /*----- app's state (variables) -----*/
+let inputVal;
+let displayVal;
+
+
 /*----- cached element references -----*/
+const counter = document.getElementById('counter');
+const inputField = document.getElementById('input-field');
+
 /*----- event listeners -----*/
+document.getElementById('plus-button').addEventListener('click', update);
+document.getElementById('minus-button').addEventListener('click', update);
+
 /*----- functions -----*/
+function init(){
+    inputVal = 0;
+    displayVal = 0;
+    render();
+};
+
+function update(event) {
+    inputVal = parseInt(inputField.value);
+    displayVal = (event.target.id === 'plus-button') ?
+        displayVal = displayVal + inputVal :
+        displayVal = displayVal - inputVal;
+    render();
+}
+
+function render() {
+    counter.innerText = displayVal;
+    if (displayVal < 0) {
+        counter.style.color = 'red';
+    } else {
+        counter.style.color = 'black';
+    }
+}
+
+init();
